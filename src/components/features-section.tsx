f78917ef42697c91a2582dd43a849aaa2279141d
Dayan Walter <dayanwalter@gmail.com>
2024-05-02 09:36:46 +0200

    Added features-section

---
+++ src/components/features-section.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function FeaturesSection() {
+  return (
+    <>
+      <div className="gap-12 py-16">
+        <div className="px-4">
+          {/* Content */}
+          <div className="">
+            {/* Heading and Text */}
+            <div className="">
+              {/* Heading and Subheading */}
+              <div className="">
+                {/* Subheading */}
+                <h4></h4>
+                {/* Heading */}
+                <h3></h3>
+              </div>
+              {/* Supporting Text */}
+              <h3 className=""></h3>
+            </div>
+          </div>
+        </div>
+        <div className="px-4"></div>
+      </div>
+    </>
+  );
+}
