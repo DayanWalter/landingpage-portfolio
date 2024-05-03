d622ee40c96854851d2cea11f125bbe35d483dba
Dayan Walter <dayanwalter@gmail.com>
2024-05-03 09:27:12 +0200

    Changed App temp

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import FeaturesSection from "./components/features-section";
 import HeroSection from "./components/hero-section";
+import QuoteSection from "./components/quote-section";
 import SocialProofSection from "./components/social-proof-section";
 import Divider from "./components/ui/divider";
 
 function App() {
   return (
     <>
-      <HeroSection />
-      <SocialProofSection />
-      <Divider />
-      <FeaturesSection />
+      {/* <HeroSection /> */}
+      {/* <SocialProofSection /> */}
+      {/* <Divider /> */}
+      {/* <FeaturesSection /> */}
+      <QuoteSection />
     </>
   );
 }
