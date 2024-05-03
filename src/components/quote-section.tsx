51421eef8a9d558597d35ad2db3d582b8866b1ef
Dayan Walter <dayanwalter@gmail.com>
2024-05-03 09:35:58 +0200

    Quote section mobile done

--- src/components/quote-section.tsx
+++ src/components/quote-section.tsx
@@ -1,25 +1,27 @@
 export default function QuoteSection() {
   return (
     <>
-      <div className="py-16">
+      <div className="bg-gray-50 py-16">
         {/* Container */}
         <div className="px-4">
           {/* Content */}
           <div>
             {/* Quote and attribution */}
-            <div className="grid gap-8">
+            <div className="grid justify-items-center gap-8">
               <img src="./companies/sisyphus.svg" alt="" />
-              <h3>
+              <h3 className="text-center text-3xl font-medium text-gray-900">
                 We've been using Untitled to kick start every new project and
                 can't imagine working without it.
               </h3>
               {/* Avatar and text */}
-              <div className="grid gap-4">
+              <div className="grid justify-items-center gap-4">
                 <img src="./quote/avatar.svg" alt="" />
                 {/* Text and supporting text */}
-                <div className="grid gap-1">
-                  <p>Candice Wu</p>
-                  <p>Product Manager, Sisyphus</p>
+                <div className="grid gap-1 text-center">
+                  <p className="text-lg font-medium text-gray-900">
+                    Candice Wu
+                  </p>
+                  <p className="text-gray-600 ">Product Manager, Sisyphus</p>
                 </div>
               </div>
             </div>
