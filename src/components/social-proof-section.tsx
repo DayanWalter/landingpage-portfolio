d8297b951c10e83b7668c4355b394230ac291bdf
Dayan Walter <dayanwalter@gmail.com>
2024-05-04 08:13:40 +0200

    Social-proof-section done

--- src/components/social-proof-section.tsx
+++ src/components/social-proof-section.tsx
@@ -10,14 +10,15 @@ export default function SocialProofSection() {
 
   return (
     <>
-      <div className="pb-16">
+      {/* Section */}
+      <div className="grid justify-center pb-16 md:py-24">
         {/* Container */}
-        <div className="grid gap-8 px-4">
-          <p className="text-center font-medium leading-6 text-gray-600">
+        <div className="grid gap-8 px-4 md:max-w-7xl md:px-8">
+          <p className="text-center font-medium text-gray-600">
             Join 4,000+ companies already growing
           </p>
           {/* Logos wrap */}
-          <ul className="grid grid-cols-2 grid-rows-3 items-center justify-items-center gap-x-8 gap-y-4">
+          <ul className="grid grid-cols-2 grid-rows-3 items-center justify-items-center gap-x-8 gap-y-4 md:grid-cols-6 md:grid-rows-1">
             {companies.map((name) => (
               <li key={name}>
                 <img src={`./companies/${name}.svg`} alt={`${name} logo`} />
