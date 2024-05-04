7a0f3d056234d8945a8c574e4eb9c4c5ed324fd0
Dayan Walter <dayanwalter@gmail.com>
2024-05-04 10:14:20 +0200

    cta-section done

--- src/components/cta-section.tsx
+++ src/components/cta-section.tsx
@@ -3,22 +3,22 @@ import { Button } from "./ui/button";
 export default function CtaSection() {
   return (
     <>
-      <div className="bg-gray-50 py-16">
+      <div className="grid justify-center bg-gray-50 py-16 md:py-24">
         {/* Container */}
-        <div className="px-4">
+        <div className="px-4 md:max-w-7xl md:px-8">
           {/* Content */}
-          <div className="grid gap-8">
+          <div className="grid gap-8 md:gap-10">
             {/* Heading and supporting text */}
-            <div className="grid gap-4 text-center">
-              <h2 className="text-3xl font-semibold text-gray-900">
+            <div className="grid gap-4 text-center md:gap-5">
+              <h2 className="text-3xl font-semibold text-gray-900 md:text-4xl">
                 Start your free trial
               </h2>
-              <p className="text-lg text-gray-500">
+              <p className="text-lg text-gray-500 md:text-xl">
                 Join over 4,000+ startups already growing with Untitled.
               </p>
             </div>
             {/* Actions */}
-            <div className="grid gap-3 ">
+            <div className="flex flex-col gap-3 md:flex-row md:justify-center">
               <Button className="font-semibold">Get started</Button>
               <Button className="font-semibold text-gray-700" variant="outline">
                 Learn more
